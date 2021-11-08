90d08d98fb6a24a96b852a5bc0bd9e4a82d43fdb
Jesus Ponce <jesus.tony.ponce@gmail.com>
2021-11-08 13:48:16 -0800

    made change to route regarding the post request

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 def instructions():
     return render_template('index.html')
 
-@main.route('/' , method=['POST'])
+@main.route('/request' , method=['POST'])
 def returnUrlofImage():
     wikiUrl = convertStr2Url(request.form['input'])
     parseDataFromWiki = getParseDatafromUrl(wikiUrl)
