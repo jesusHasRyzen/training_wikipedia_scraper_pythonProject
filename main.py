e6e08f01f1e2ab50de3be4e55ab6630680a64177
Jesus Ponce <jesus.tony.ponce@gmail.com>
2021-11-08 20:47:59 -0800

    got rid of str() func on get(src)

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def testIfImagesExist(link):
     return True
 def getLinktoImage(firstLink):
     # get the link information of link leading to the image associated with input
-    firstImageStr = str(firstLink.get("src"))
+    firstImageStr = firstLink.get("src")
     return firstImageStr
     # if link does not include https:, then include it to make it a complete link
     firstImageStr = "https:" + firstImageStr
