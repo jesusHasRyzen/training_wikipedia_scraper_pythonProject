e7fe1de27ca448908f8772fc65594dfe72fe52a4
Jesus Ponce <jesus.tony.ponce@gmail.com>
2021-11-15 10:25:33 -0800

    changed the main page and got rid of the main project code

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 # it works
 import base64
 
+
+
 import requests
+#imported beautifulSoup to help parse the data getting passed when
+# user makes request in json format
 from bs4 import BeautifulSoup
+#imported flask to work with the deploying the program online.
 from flask import Flask, render_template, request
 import json
-from base64 import decodebytes
-from PIL import Image
-from io import BytesIO
-# in the form of a string which will be added to the given url
-# used to look up quite honestly anything on wikipedia
+
+
 main = Flask(__name__)
 
 
-uname = ""
 images = ""
 
 if __name__ == '__main__':
@@ -25,44 +26,6 @@ def instructions():
     return render_template('index.html')
 
 
-
-
-@main.route('/loggedIN', methods = ['post'])
-def instructions2():
-    pws = request.form["psw"]
-    urlEncrypt = 'https://realpython-example-app2.herokuapp.com/?username='+pws
-    # returnFromRequest = requests.get(urlEncrypt)
-    # encryptPW = decode_to_string(returnFromRequest)
-    pws = requests.get(urlEncrypt).content
-    uname = request.form["uname"]
-
-
-    urlRules = 'https://team-anything-microservice.herokuapp.com/get_rules'
-    jsonresponse = requests.get(urlRules).json()
-    rules = jsonresponse['deal']
-    objective = jsonresponse['objective']
-    pack = jsonresponse['pack']
-    rank =  jsonresponse['rank']
-    score = jsonresponse['score']
-    link = jsonresponse['youtube']
-    urlImages = 'https://team-anything-microservice.herokuapp.com/get_images'
-    # images = requests.get(urlImages).json()
-    # byte_array = bytes(images)
-    # images_decoded = base64.decodebytes(byte_array)
-    # images_decoded = BytesIO(images_decoded)
-
-
-
-    return render_template("loggedIn.html", name = uname, rules = rules, objective = objective, pack = pack , rank = rank, score = score, link = link, pws = pws)
-
-# @main.route('/getImages')
-# def instructions3():
-#     urlImages = 'https://team-anything-microservice.herokuapp.com/get_images'
-#
-#     # returnFromRequest = requests.get(urlEncrypt)
-#     # encryptPW = decode_to_string(returnFromRequest)
-#     return requests.get(urlImages).content
-
 @main.route('/' , methods =['post'])
 def returnUrlofImage():
     str2 = "ME"
@@ -73,10 +36,10 @@ def returnUrlofImage():
         parseDataFromWiki = getParseDatafromUrl(wikiUrl)
         htmltagImage = getAllImagesFromUrl(parseDataFromWiki)
 
-        bs4classTemp = getImageData(parseDataFromWiki, str2)
+        bs4classTemp = getImageData(parseDataFromWiki)
         doesExist = testIfImagesExist(htmltagImage)
         if doesExist:
-            str2 = getLinktoImage(bs4classTemp)
+            str2 = getLinktoImage(bs4classTemp, str2)
             return str2
     return "does not exist"
 
@@ -91,17 +54,13 @@ def getParseDatafromUrl(url):
     # scrape webpage
     soup = BeautifulSoup(page.content, 'html.parser')
     return soup
-def getImageData(htmlParseData, input):
+def getImageData(htmlParseData):
     # gets all links in html that are tagged images
     images = htmlParseData.find_all("img")
     # goes thru the images link saved and gets the source the images
     # for loop to go thru all the list saved.
     # for item in images:
     # 	print(item['src'])
-    for image in images:
-        if input in image:
-            firstImage = image
-            return firstImage
     firstImage = images[3]
     return firstImage
 def getAllImagesFromUrl(htmlParseData):
