f639272e1812af49cec5bbceec8cf1b4ff9d49af
Jesus Ponce <jesus.tony.ponce@gmail.com>
2021-11-15 18:49:34 -0800

    got rid of attempt to debug

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
 # it works
 import base64
 
-import logging
-from http.client import HTTPConnection
 
-log = logging.getLogger('urllib3')
-log.setLevel(logging.DEBUG)
-
-
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-log.addHandler(ch)
-HTTPConnection.debuglevel = 1
 # to make a request in order to obtain data from wiki
 import requests
 
